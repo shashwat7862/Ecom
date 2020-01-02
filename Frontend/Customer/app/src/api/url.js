85063eaa68534c5f2dff26c66ed13eddef1608f3
rohitgoyal9293 <goyal.rohit9293@gmail.com>
2020-01-02 23:53:49 +0530

    header fixed

--- Frontend/Customer/app/src/api/url.js
+++ Frontend/Customer/app/src/api/url.js
@@ -1,7 +1,7 @@
 
 
-const Baseurl = "//localhost:8080";
+//const Baseurl = "//localhost:8080";
 // const Baseurl = "http://13.232.88.106:8080";
-// const Baseurl = "http://12c7adf6.ngrok.io";
+const Baseurl = "http://b703c93a.ngrok.io";
 
 export default Baseurl
