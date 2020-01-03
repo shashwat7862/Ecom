971202340ed518b5865956a702469b8c331cb818
shashwat <shashwat.company@gmail.com>
2020-01-03 23:58:45 +0530

    Change Base URL

--- Frontend/Admin/app/src/assets/data/url.js
+++ Frontend/Admin/app/src/assets/data/url.js
@@ -1,7 +1,7 @@
 
 
-const Baseurl = "//localhost:8080";
-// const Baseurl = "http://13.232.88.106:8080";
-
+// const Baseurl = "//localhost:8080";
+const Baseurl = "http://2.56.213.241:8080";
+// const Baseurl = "http://12c7adf6.ngrok.io";
 
 export default Baseurl
