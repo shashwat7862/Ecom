07612b2665a461c30cb77579fe26026613090751
shashwat <shashwat.company@gmail.com>
2020-01-04 13:27:04 +0530

    edit profile image issue fixed

--- Frontend/Customer/app/src/api/url.js
+++ Frontend/Customer/app/src/api/url.js
@@ -1,7 +1,7 @@
 
 
-//const Baseurl = "//localhost:8080";
+const Baseurl = "//localhost:8080";
 // const Baseurl = "http://13.232.88.106:8080";
-const Baseurl = "https://d5ed8219.ngrok.io";
+// const Baseurl = "https://d5ed8219.ngrok.io";
 
 export default Baseurl
